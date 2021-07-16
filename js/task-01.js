c29b39dff15f21e163614a9f67a2a25de51fd41d
Eugene-Bocharov <BocharovWork12@gmail.com>
2021-07-16 18:57:50 +0000

    hw-7

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const refs = {
+    ul: document.querySelector('#categories'),
+    li_item: document.querySelectorAll('.item'),
+
+}
+
+console.log('В списке ' + refs.li_item.length + ' категории')
+
